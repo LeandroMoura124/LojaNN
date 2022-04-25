c027b25855c5d34ef92c2fa17c5d1289952685cb
3etimds <3etimds@LAB2-11>
2022-04-25 09:29:45 -0300

    Adicionar arquivos de projeto.

---
+++ LojaNN/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNN.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "QUERO O PC DE VOLTA";
+            ViewBag.BG = "BAG GANGSTER";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewBag.CONT = "Eu adoro a professora Heliene"; 
+
+            return View();
+        }
+
+        public ActionResult Aluno()
+        {
+            ViewBag.ID = "1";
+            ViewBag.NOME = "Filipe";
+            ViewBag.SEXO = "Celta vermelho rebaixado";
+            return View();
+        }
+    }
+}
---
+++ LojaNN/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNN.Models
+{
+    public class Pessoa
+    {
+        public int idPess { get; set; }
+
+        public string nomePess { get; set; }
+
+        public string sexoPess { get; set; }
+
+
+    }
+
+}
